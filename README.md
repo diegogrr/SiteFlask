4783aed8a4bfee2201d5fbb8e79959f78c9f609c
Diego Garrido <diego.grr@gmail.com>
2022-05-02 00:08:43 -0300

    some improvements

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Learning Flask
 
 ## References
-freeCodeCamp.org "Learn Flask for Python - Full Tutorial" Link:https://youtu.be/Z1RJmh_OqeA
+freeCodeCamp.org, "Learn Flask for Python - Full Tutorial". Link: https://youtu.be/Z1RJmh_OqeA
+freeCodeCamp.org, "Web Programming with Flask - Intro to Computer Science - Harvard's CS50 (2018)". Link: https://youtu.be/zdgYw-3tzfI
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import csv
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -25,16 +26,20 @@ def index():
         task_content = request.form['content']
         new_task = Todo(content=task_content)
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
-
+        if not task_content:
+            msg = 'Please, write your task.'
+            tasks = Todo.query.order_by(Todo.date_created).all()
+            return render_template('index.html', tasks=tasks, msg=msg)
+        else: 
+            try:
+                db.session.add(new_task)
+                db.session.commit()
+                return redirect('/')
+            except:
+                return 'There was an issue adding your task'
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        return render_template('index.html', tasks=tasks, msg='')
 
 @app.route('/delete/<int:id>')
 def delete_task(id):
@@ -70,6 +75,22 @@ def contacts():
 @app.route('/users/<my_user>')
 def users(my_user):
     return render_template('users.html', my_user=my_user)
+
+@app.route('/register', methods=['POST'])
+def register():
+    if not request.form.get('name') or not request.form.get('email'):
+        return render_template('failure.html')
+    with open('registered.csv', 'a', newline='', encoding='utf-8') as file: # 'a' means 'append'
+        writer = csv.writer(file)
+        writer.writerow((request.form.get('name'), request.form.get('email')))
+    return render_template('success.html')
+
+@app.route('/registered')
+def registered():
+    with open('registered.csv', 'r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file, delimiter=',')
+        registered = list(reader)
+    return render_template('registered.html', registered=registered)
 
 # Execute the server function
 if __name__ == "__main__":
